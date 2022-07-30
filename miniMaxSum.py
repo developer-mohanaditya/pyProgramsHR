d924e1888feb2995b2498d43221515ef265e4a0a
Mohan Aditya Sadhanala <100233499+developer-mohanaditya@users.noreply.github.com>
2022-07-30 17:42:56 +0530

    Update miniMaxSum.py

--- miniMaxSum.py
+++ miniMaxSum.py
@@ -1,3 +1,35 @@
+# #!/bin/python3
+
+# import math
+# import os
+# import random
+# import re
+# import sys
+
+# #
+# # Complete the 'miniMaxSum' function below.
+# #
+# # The function accepts INTEGER_ARRAY arr as parameter.
+# #
+
+# def miniMaxSum(arr):
+#     # Write your code here
+#     arr.sort()
+#     minArr = arr[:-1]
+#     maxArr = arr[1:]
+#     minSum = maxSum = 0
+#     for i in range(len(minArr)):
+#         minSum += minArr[i]
+#     for j in range(len(maxArr)):
+#         maxSum += maxArr[j]    
+#     print(minSum, maxSum)
+    
+# if __name__ == '__main__':
+
+#     arr = list(map(int, input().rstrip().split()))
+
+#     miniMaxSum(arr)
+
 #!/bin/python3
 
 import math
@@ -17,15 +49,16 @@ def miniMaxSum(arr):
     arr.sort()
     minArr = arr[:-1]
     maxArr = arr[1:]
-    minSum = maxSum = 0
+    sumArr = [0, 0]
     for i in range(len(minArr)):
-        minSum += minArr[i]
-    for j in range(len(maxArr)):
-        maxSum += maxArr[j]    
-    print(minSum, maxSum)
+        sumArr[0] += minArr[i]
+        sumArr[1] += maxArr[i]
+    print(sumArr[0], sumArr[1])
     
+
 if __name__ == '__main__':
 
     arr = list(map(int, input().rstrip().split()))
 
     miniMaxSum(arr)
+
